e304afabc1c87864840be7eb1da449ba5c2fe2bb
forteminds2582 <joshholmestit4@gmail.com>
2023-02-16 14:13:12 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main functions
+ *
+ * Return: always 0
+ */
+
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle");
-		return 0;
+	puts("\"Programming is like building a multilingual puzzle);
+		return (0);
 }
