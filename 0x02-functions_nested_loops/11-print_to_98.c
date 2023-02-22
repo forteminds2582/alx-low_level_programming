1adfddaf2366f3d1edae2e312fe11b92293f6c23
forteminds2582 <joshholmestit4@gmail.com>
2023-02-22 07:11:43 -0800

    tasl 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,26 +15,32 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d ,", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n < 0)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d ,", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d ,", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
