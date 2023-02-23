ce9e4c6177827979b702fc6e715c24bd55b53444
forteminds2582 <joshholmestit4@gmail.com>
2023-02-23 15:10:34 -0800

    task 2 Collaboration is multiplication mandatory

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - funtion that multiply two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: return value of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
